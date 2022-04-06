20dedcaaa0c49d19f7a77bf8d95f766bdf89ff88
Securedill <35273759+Securedill@users.noreply.github.com>
2022-04-06 09:28:55 +0200

    checking structure

--- app/java/ru/skillbranch/skillarticles/ui/custom/CheсkableImageView.kt
+++ app/java/ru/skillbranch/skillarticles/ui/custom/CheсkableImageView.kt
@@ -5,7 +5,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.widget.Checkable
 
-class CheckableImageView @JvmOverloads constructor(
+class CheсkableImageView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
